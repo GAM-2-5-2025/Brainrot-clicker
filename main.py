97478cfd2c6bbdf69421ddb75aa1013cd1dbcc48
Dragoonz2 <patrik.klovar.lukaric@gmail.com>
2025-05-21 19:47:40 +0200

    Update main.py
    slika za main button 🙏

--- main.py
+++ main.py
@@ -7,16 +7,23 @@ color_light = (170, 170, 170)
 color_dark = (100, 100, 100)
 black=(0,0,0)
 white = (255, 255, 255)
+image_size = (200, 200)
+
 
 screen = pygame.display.set_mode((w, h))
 pygame.display.set_caption('Brainrot Clicker')
-color = (0, 0, 255)
-
+brain = pygame.image.load('brainrot.png').convert_alpha()
+brain = pygame.transform.scale(brain, image_size)
+brain_hover = pygame.image.load('brainrot_light.png').convert_alpha()
+brain_hover = pygame.transform.scale(brain_hover, image_size)
+brain_rect = brain.get_rect(topleft=(750, 350))
+brain_hover_rect = brain_hover.get_rect(topleft=(750, 350))
 
 def main():
     buy_1 = 0
     buy_2 = 0
     buy_3 = 0
+
 
 
     bought_multiplier_1 = 1.5
@@ -30,11 +37,16 @@ def main():
     multiplier=1
     clock = pygame.time.Clock()
     while run:
-        
         dt = clock.tick(60) / 1000.0  # Convert milliseconds to seconds
         value_1= 10 + buy_1 * bought_multiplier_1
         value_2 = 100 + buy_2 * bought_multiplier_2
         value_3 = 1000 + buy_3 * bought_multiplier_3
+
+        s_value1 =  value_1 * 0.5
+        s_value2 =  value_2 * 0.25
+        s_value3 =  value_3 * 0.1
+
+
         mouse = pygame.mouse.get_pos()
 
 
@@ -42,7 +54,7 @@ def main():
             if ev.type == pygame.QUIT:
                 run = False
 
-            if ev.type == pygame.MOUSEBUTTONDOWN:
+            if ev.type == pygame.MOUSEBUTTONDOWN and ev.button == 1:
                 mouse = pygame.mouse.get_pos()
                 now = time.time()
 
@@ -71,6 +83,23 @@ def main():
                     br -= value_2
                     buy_2 += 1
 
+            elif ev.type == pygame.MOUSEBUTTONDOWN and ev.button == 3:
+
+                if 1400 <= mouse[0] <= 1400 + 150 and 450 <= mouse[1] <= 450 + 40 and buy_1 >= 1:
+                    br += s_value1
+                    multiplier -= 1
+                    buy_1 -= 1
+
+                if 1400 <= mouse[0] <= 1400 + 150 and 650 <= mouse[1] <= 650 + 40 and buy_3 > 0:
+                    br += s_value3
+                    buy_3 -= 1
+
+                if 1400 <= mouse[0] <= 1400 + 150 and 550 <= mouse[1] <= 550 + 40 and buy_2 > 0:
+                    br += s_value2
+                    buy_2 -= 1
+
+
+
         # Passive income
         if buy_2 > 0:
             br += buy_2 * 0.5 * dt
@@ -78,16 +107,20 @@ def main():
         if buy_3 > 0:
             br += buy_3 * 15 * dt
 
-        screen.fill(color)
-
 
         screen.fill((255, 255, 255))
+        l=['', 'k', ' million', ' billion', ' trillion', ' quadrillion']
+        broj_potencije=(len(str(int(br)))-1)//3
+        broj_koji_se_prikazuje_na_ekranu=round(br/(10**(broj_potencije*3)), 1)
+        broj_koji_se_prikazuje_na_ekranu=str(broj_koji_se_prikazuje_na_ekranu)
+        broj_koji_se_prikazuje_na_ekranu+=l[broj_potencije]
+
 
         # Text through GUI
         myFont1 = pygame.font.SysFont("Comic Sans", 100)
         myFont2 = pygame.font.SysFont("Comic Sans", 20)
 
-        NumLabel = myFont1.render(f"Brainrot: {str(round(br, 1))}", 1, black)
+        NumLabel = myFont1.render(f"Brainrot: {broj_koji_se_prikazuje_na_ekranu}", 1, black)
         touch = myFont2.render(f"The Mouse: {multiplier}", 1, white)
         cursor = myFont2.render(f"The Cursor: {buy_2}", 1, white)
         skibidi_toilet = myFont2.render(f"The Skibidi: {buy_3}", 1, white)
@@ -100,11 +133,10 @@ def main():
 
         # if mouse is hovered on a button it
         # changes to lighter shade
-        if 750 <= mouse[0] <= 750 + 200 and 350 <= mouse[1] <= 350 + 200:
-            pygame.draw.rect(screen, color_light, [750, 350, 200, 200])
-
+        if brain_rect.collidepoint(mouse):
+            screen.blit(brain_hover, brain_hover_rect)
         else:
-            pygame.draw.rect(screen, color_dark, [750, 350, 200, 200])
+            screen.blit(brain, brain_rect)
 
 
         if 1400 <= mouse[0] <= 1400 + 150 and 450 <= mouse[1] <= 450 + 40:
