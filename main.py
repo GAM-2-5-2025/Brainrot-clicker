142f4556f638873869df53f4eec3e13dc006bc0b
Dragoonz2 <patrik.klovar.lukaric@gmail.com>
2025-06-10 08:48:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ color_light = (170, 170, 170)
 color_dark = (100, 100, 100)
 black=(0,0,0)
 white = (255, 255, 255)
-image_size = (200, 200)
+image_size = (400, 400)
 image_size_1 = (200, 50)
 window_size = (1600, 900)
 
@@ -22,18 +22,24 @@ brain = pygame.image.load('brainrot.png').convert_alpha()
 brain = pygame.transform.scale(brain, image_size)
 brain_hover = pygame.image.load('brainrot_light.png').convert_alpha()
 brain_hover = pygame.transform.scale(brain_hover, image_size)
-brain_rect = brain.get_rect(topleft=(750, 350))
-brain_hover_rect = brain_hover.get_rect(topleft=(750, 350))
+brain_rect = brain.get_rect(topleft=(600, 270))
+brain_hover_rect = brain_hover.get_rect(topleft=(600, 270))
 
 button = pygame.image.load('button.png').convert_alpha()
 button_hover = pygame.image.load('button.png').convert_alpha()
-button_rect = brain.get_rect(topleft=(1350, 450))
-button_hover_rect = brain_hover.get_rect(topleft=(1350, 450))
 
-button_rect1 = brain.get_rect(topleft=(1350, 550))
-button_hover_rect1 = brain_hover.get_rect(topleft=(1350, 550))
+button_rect = brain.get_rect(topleft=(1350, 350))
+button_hover_rect = brain_hover.get_rect(topleft=(1350, 350))
+button_rect1 = brain.get_rect(topleft=(1350, 450))
+button_hover_rect1 = brain_hover.get_rect(topleft=(1350, 450))
+button_rect2 = brain.get_rect(topleft=(1350, 550))
+button_hover_rect2 = brain_hover.get_rect(topleft=(1350, 550))
+button_rect3 = brain.get_rect(topleft=(1350, 550))
+button_hover_rect3 = brain_hover.get_rect(topleft=(1350, 550))
 
 skibidi_toilet_img = pygame.image.load("skibidi_toilet.png").convert_alpha()
+tralalero_tralala_img = pygame.image.load("tralalero_tralala.png").convert_alpha()
+ltf_img = pygame.image.load("ltf.png").convert_alpha()
 
 
 
@@ -138,12 +144,14 @@ def main():
         myFont2 = pygame.font.SysFont("Comic Sans", 20)
 
         NumLabel = myFont1.render(f"Brainrot: {broj_koji_se_prikazuje_na_ekranu}", 1, black)
-        touch = myFont2.render(f"The Cursor: {buy_1}", 1, white)
+        touch = myFont2.render(f"Cursor: {buy_1}", 1, white)
         touch_price = myFont2.render(f"Price: {value_1}", 1, white)
-        cursor = myFont2.render(f"The Skibidi: {buy_2}", 1, white)
+        cursor = myFont2.render(f"Skibidi: {buy_2}", 1, white)
         cursor_price = myFont2.render(f"Price: {value_2}", 1, white)
-        skibidi_toilet = myFont2.render(f"The Crocodilo: {buy_3}", 1, white)
+        skibidi_toilet = myFont2.render(f"Tralalero: {buy_3}", 1, white)
         skibidi_toilet_price = myFont2.render(f"Price: {value_3}", 1, white)
+        ltf = myFont2.render(f"Low Taper Fade: {buy_3}", 1, white)
+        ltf_price = myFont2.render(f"Price: {value_3}", 1, white)
 
 
 
@@ -166,41 +174,60 @@ def main():
             screen.blit(button_hover, button_hover_rect)
         else:
             screen.blit(button, button_rect)
-
+#skibidi toilet buttton
         if brain_rect.collidepoint(mouse):
             screen.blit(button_hover, button_hover_rect1)
         else:
             screen.blit(button, button_rect1)
-
-
-
-        if 1400 <= mouse[0] <= 1400 + 150 and 650 <= mouse[1] <= 650 + 50:
-                pygame.draw.rect(screen, color_light, [1400, 650, 150, 50])
-
+#tralalero tralala button
+        if brain_rect.collidepoint(mouse):
+            screen.blit(button_hover, button_hover_rect2)
         else:
-            pygame.draw.rect(screen, color_dark, [1400, 650, 150, 50])
+            screen.blit(button, button_rect2)
+#low taper fade button
+        if brain_rect.collidepoint(mouse):
+            screen.blit(button_hover, button_hover_rect3)
+        else:
+            screen.blit(button, button_rect3)
 
 
-        screen.blit(skibidi_toilet_img, (1334, 544))
+        screen.blit(skibidi_toilet_img, (1334, 444))
+        screen.blit(tralalero_tralala_img, (1334, 544))
+        screen.blit(ltf_img, (1334, 644))
 
-        screen.blit(touch, (1400, 450))
-        screen.blit(cursor, (1400, 550))
-        screen.blit(skibidi_toilet, (1400, 650))
-        screen.blit(touch_price, (1400, 470))
-        screen.blit(cursor_price, (1400, 570))
-        screen.blit(skibidi_toilet_price, (1400, 670))
+        screen.blit(touch, (1400, 350))
+        screen.blit(cursor, (1400, 450))
+        screen.blit(skibidi_toilet, (1400, 550))
+        screen.blit(ltf, (1400, 650))
+        
+        screen.blit(touch_price, (1400, 370))
+        screen.blit(cursor_price, (1400, 470))
+        screen.blit(skibidi_toilet_price, (1400, 570))
+        screen.blit(ltf_price, (1400, 670))
 
         if br < value_1:
-            cursor_no_money = myFont2.render(f"The Cursor: {buy_1}", 1, black)
+            cursor_no_money = myFont2.render(f"Cursor: {buy_1}", 1, black)
             cursor_price_no_money = myFont2.render(f"Price: {value_1}", 1, black)
-            screen.blit(cursor_no_money, (1400, 450))
-            screen.blit(cursor_price_no_money, (1400, 470))
+            screen.blit(cursor_no_money, (1400, 350))
+            screen.blit(cursor_price_no_money, (1400, 370))
 
         if br < value_2:
-            skibidi_no_money = myFont2.render(f"The Skibidi: {buy_2}", 1, black)
+            skibidi_no_money = myFont2.render(f"Skibidi: {buy_2}", 1, black)
             skibidi_price_no_money = myFont2.render(f"Price: {value_2}", 1, black)
+            screen.blit(skibidi_no_money, (1400, 450))
+            screen.blit(skibidi_price_no_money, (1400, 470))
+
+        if br < value_3:
+            skibidi_no_money = myFont2.render(f"Tralalero: {buy_3}", 1, black)
+            skibidi_price_no_money = myFont2.render(f"Price: {value_3}", 1, black)
             screen.blit(skibidi_no_money, (1400, 550))
             screen.blit(skibidi_price_no_money, (1400, 570))
+        
+        if br < value_4:
+            cursor_no_money = myFont2.render(f"Low Taper Fade: {buy_4}", 1, black)
+            cursor_price_no_money = myFont2.render(f"Price: {value_4}", 1, black)
+            screen.blit(ltf_no_money, (1400, 650))
+            screen.blit(ltf_price_no_money, (1400, 670))
 
         pygame.display.update()
 
